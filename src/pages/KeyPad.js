470eac83769520c299d0b06f24e8fe38313a0cfb
jade <s3839597@student.rmit.edu.au>
2023-07-06 16:11:12 +1000

    key pad fix UI

--- src/pages/KeyPad.js
+++ src/pages/KeyPad.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import arrow from './arrow.svg';
-import TextField from '@mui/material/TextField';
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
@@ -23,43 +22,43 @@ function KeyPad() {
                     Find GP Clinic by <br />
                     Post Code
                 </h1>
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <div className="overlap">
+                <button className="text-field"></button>
+                <button className="overlap">
                     <div className="text-wrapper">Confirm</div>
-                </div>
-                <div className="overlap-group">
+                </button>
+                <button className="overlap-group">
                     <div className="text-wrapper-2">4</div>
-                </div>
-                <div className="div-wrapper">
+                </button>
+                <button className="div-wrapper">
                     <div className="text-wrapper-3">2</div>
-                </div>
-                <div className="overlap-2">
+                </button>
+                <button className="overlap-2">
                     <div className="text-wrapper-4">7</div>
-                </div>
-                <div className="overlap-3">
+                </button>
+                <button className="overlap-3">
                     <div className="text-wrapper-2">5</div>
-                </div>
-                <div className="overlap-4">
+                </button>
+                <button className="overlap-4">
                     <div className="text-wrapper-4">8</div>
-                </div>
-                <div className="overlap-5">
+                </button>
+                <button className="overlap-5">
                     <div className="text-wrapper-2">6</div>
-                </div>
-                <div className="overlap-6">
+                </button>
+                <button className="overlap-6">
                     <div className="text-wrapper-5">9</div>
-                </div>
-                <div className="overlap-7">
+                </button>
+                <button className="overlap-7">
                     <div className="text-wrapper-2">0</div>
-                </div>
-                <div className="overlap-8">
+                </button>
+                <button className="overlap-8">
                     <div className="text-wrapper-6">3</div>
-                </div>
+                </button>
                 <button className="arrow-overlap" onClick={back}>
                     <img src={arrow} className="arrow" alt="arrow" />
                 </button>
-                <div className="overlap-9">
+                <button className="overlap-9">
                     <div className="text-wrapper-2">1</div>
-                </div>
+                </button>
             </div>
         </div>
     );
